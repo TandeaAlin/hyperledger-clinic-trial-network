249d89f2e0a9305ab432c47e32a46d2017b215b0
Alin Tandea <alin3011@gmail.com>
2018-04-02 20:56:07 +0300

    [cleanup][trial, patient, organisation model definition]

--- clinic-trial-network/create-bna.sh
+++ clinic-trial-network/create-bna.sh
@@ -1,7 +1,11 @@
+./fabric-tools/stopFabric.sh
+./fabric-tools/teardownFabric.sh
+./fabric-tools/startFabric.sh
+./fabric-tools/createPeerAdminCard.sh
+composer card delete --name admin@clinic-trial-network
 composer archive create -t dir -n .
 composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName clinic-trial-network
 composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile clinic-trial-network@0.0.1.bna --file networkadmin.card
 composer card import --file networkadmin.card
 composer network ping --card admin@clinic-trial-network
-composer-rest-server
 composer-rest-server -c admin@clinic-trial-network -n never -w true
--- clinic-trial-network/lib/logic.js
+++ clinic-trial-network/lib/logic.js
@@ -1,1 +1,24 @@
 'use strict';
+function registerTrialTransaction(trialRequest){
+    var factory = getFactory();
+    var namespace_trial = 'ro.utcluj.clinictrial.trial'
+    var namespace_person = 'ro.utcluj.clinictrial.base'
+
+    var trial = factory.newResource(namespace_trial, 'Trial', trialRequest.idTrial);
+    trial.details = trialRequest.details;
+    trial.status = 'REGISTERED';
+
+    //add the relationship to the Researcher that created the clinic trial
+    trial.organiser = factory.newRelationship(namespace_person, 'Researcher',
+                            trialRequest.organiser.getIdentifier());
+
+    // save the order
+    return getAssetRegistry(trial.getFullyQualifiedType())
+            .then(function (assetRegistry) {
+                      return assetRegistry.add(trial);
+  })
+}
+
+function setupMockData(){
+    
+}
