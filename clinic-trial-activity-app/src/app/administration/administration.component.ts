2b2219b7ce9da7b9e9a0bb5b2a6038e7a8a6743c
Alin Tandea <alin3011@gmail.com>
2018-04-14 18:25:47 +0300

    [added patient view control buttons][implemented delete]

--- clinic-trial-activity-app/src/app/administration/administration.component.ts
+++ clinic-trial-activity-app/src/app/administration/administration.component.ts
@@ -2,7 +2,8 @@ import { Component, ViewChild, OnInit } from '@angular/core';
 import {MatPaginator, MatTableDataSource} from '@angular/material';
 import { Patient } from '../model/ro.utcluj.clinictrial.base';
 import { PatientService } from '../service/patient.service'
-
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { OnChanges } from '@angular/core';
 
 @Component({
   selector: 'administration-component',
@@ -12,15 +13,28 @@ export class AdministrationComponent implements OnInit{
 
   private allPatients : Patient[] = [];
   private errorMessage;
+  navigationSubscription;
   allPatientsDataSource : MatTableDataSource<Patient>;
-  private displayedColumns = ['PatientID', 'Name', 'Gender', 'Birthdate'];
+  private displayedColumns = ['PatientID', 'Name', 'Gender', 'Birthdate', 'View', 'Edit', 'Delete'];
   @ViewChild(MatPaginator) paginator: MatPaginator;
   constructor(
-    private _patientService : PatientService
-  ){}
+    private _patientService : PatientService,
+    private _router : Router,
+    private _route : ActivatedRoute,
+  ){
+    this.navigationSubscription = this._router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initalise the component
+      if (e instanceof NavigationEnd) {
+        this.loadAllPatients();
+      }
+    });
+  }
 
   ngOnInit(){
-    this.loadAllPatients();
+  }
+
+  ngOnChanges(){
+
   }
 
   ngAfterViewInit() {
@@ -31,6 +45,7 @@ export class AdministrationComponent implements OnInit{
     return this._patientService.getAll()
       .toPromise()
       .then((result) => {
+      this.allPatients = []; 
 			this.errorMessage = null;
       console.log(result)
       result.forEach(asset => {
@@ -50,4 +65,28 @@ export class AdministrationComponent implements OnInit{
         }
     });
   }
+
+  deletePatient(patient){
+    console.log(patient.idPatient);
+    this._patientService.deleteAsset(patient.idPatient)
+      .toPromise()
+		  .then(() => {
+        this.errorMessage = null;
+        this._router.navigate(['administration'])		  
+      })
+		  .catch((error) => {
+        if(error == 'Server error'){
+				    alert("Could not connect to REST server. Please check your configuration details")
+			  }
+			  else if(error == '404 - Not Found'){
+				  alert("404 - Could not find API route. Please check your available APIs.")
+			  }
+			  else{
+				  alert(error);
+			  }
+    });
+  }
+
+
 }
+
