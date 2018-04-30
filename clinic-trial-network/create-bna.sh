cab1bd8c04b39f54dfdf9b59b359c1a56edd6dbd
Alin Tandea <alin3011@gmail.com>
2018-04-30 16:36:35 +0300

    [Composer version update -> 0.19.2][nothing works]

--- clinic-trial-network/create-bna.sh
+++ clinic-trial-network/create-bna.sh
@@ -2,10 +2,12 @@
 ./fabric-tools/teardownFabric.sh
 ./fabric-tools/startFabric.sh
 ./fabric-tools/createPeerAdminCard.sh
-composer card delete --name admin@clinic-trial-network
+
+composer card delete --card admin@clinic-trial-network
+
 composer archive create -t dir -n .
-composer runtime install --card PeerAdmin@hlfv1 --businessNetworkName clinic-trial-network
-composer network start --card PeerAdmin@hlfv1 --networkAdmin admin --networkAdminEnrollSecret adminpw --archiveFile clinic-trial-network@0.0.1.bna --file networkadmin.card
+composer network install --card PeerAdmin@hlfv1 --archiveFile clinic-trial-network@0.0.1.bna
+composer network start --networkName clinic-trial-network --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
 composer card import --file networkadmin.card
 composer network ping --card admin@clinic-trial-network
 composer-rest-server -c admin@clinic-trial-network -n never -w true
--- clinic-trial-network/lib/logic.js
+++ clinic-trial-network/lib/logic.js
@@ -1,24 +1,31 @@
 'use strict';
+
+/**
+ * Register a new trial, add the required relationships and set the trial status
+ * @param {ro.utcluj.clinictrial.RegisterTrialTransaction} trialRequest - Transaction
+ * to be processed
+ * @transaction
+ */
 function registerTrialTransaction(trialRequest){
     var factory = getFactory();
-    var namespace_trial = 'ro.utcluj.clinictrial.trial'
-    var namespace_person = 'ro.utcluj.clinictrial.base'
+    //namespace definitions
+    var namespace = 'ro.utcluj.clinictrial'
 
-    var trial = factory.newResource(namespace_trial, 'Trial', trialRequest.idTrial);
+
+    var trial = factory.newResource(namespace_trial, '.Trial', trialRequest.idTrial);
     trial.details = trialRequest.details;
     trial.status = 'REGISTERED';
 
     //add the relationship to the Researcher that created the clinic trial
-    trial.organiser = factory.newRelationship(namespace_person, 'Researcher',
+    trial.organiser = factory.newRelationship(namespace_person, '.Researcher',
                             trialRequest.organiser.getIdentifier());
 
-    // save the order
+    // save the asset
     return getAssetRegistry(trial.getFullyQualifiedType())
             .then(function (assetRegistry) {
                       return assetRegistry.add(trial);
   })
 }
-
 function setupMockData(){
-    
+
 }
