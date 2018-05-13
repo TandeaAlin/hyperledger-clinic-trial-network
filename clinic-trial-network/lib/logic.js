63754d225587786e9443405dd40eed1985c1b5f8
Alin Tandea <alin3011@gmail.com>
2018-05-13 21:58:32 +0300

    [:|]

--- clinic-trial-network/lib/logic.js
+++ clinic-trial-network/lib/logic.js
@@ -2,14 +2,24 @@
 
 /**
  * Register a new trial, add the required relationships and set the trial status
- * @param {ro.utcluj.clinictrial.trial.RegisterTrialTransaction} trialRequest - Transaction
+ * @param {ro.utcluj.clinictrial.trial.RegisterTrialTransaction} tx - Transaction
  * to be processed
  * @transaction
  */
-function registerTrialTransaction(trialRequest) {
-  //TODO
-}
+async function registerTrialTransaction(tx) {
+  console.log('Registering a new clinical study...')
 
+  const factory = getFactory();
+  const NS_TRIAL = 'ro.utcluj.clinictrial.trial';
+  const NS_ORG = 'ro.utcluj.clinictrial.organisation';
+
+  const trial = factory.newResource(NS_TRIAL, 'TRIAL', tx.idTrial);
+  trial.studyName = tx.studyName;
+  trial.status = 'REGISTERED';
+  trial.organiser = factory.newRelationship(NS_ORG, 'ResearchSite', tx.organiser.getIdentifier());
+  const registry = await getAssetRegistry(trial.getFullyQualifiedType());
+  await registry.add(trial);
+}
 
 function setupMockData() {
 
