d3d059dd663264f946dad323d24e8a4148a56b08
Alin Tandea <alin3011@gmail.com>
2018-07-09 01:34:30 +0300

    [forgot dis]

--- clinic-trial-network/start-multi-rest-server.sh
+++ clinic-trial-network/start-multi-rest-server.sh
@@ -2,14 +2,24 @@ export COMPOSER_PROVIDERS='{
   "github": {
     "provider": "github",
     "module": "passport-github",
-    "clientID": "23352dfd6e39b8f45e55",
-    "clientSecret": "1cd9ef526cd23360ebb80c457881cd1fa560f9c9",
+    "clientID": "[ID]",
+    "clientSecret": "[SECRET]",
     "authPath": "/auth/github",
     "callbackURL": "/auth/github/callback",
     "successRedirect": "http://localhost:4200?loggedIn=true",
     "failureRedirect": "/"
   }
 }'
+
+export COMPOSER_DATASOURCES='{
+    "db": {
+        "name": "mydb",
+        "connector": "mongodb",
+        "database": "mydb",
+        "host": "localhost"
+    }
+}'
+
 
 
 
